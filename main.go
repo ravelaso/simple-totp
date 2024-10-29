c00881c305eaae837ec5ede6a9eb3ceeba4ca8da
Andres Ravelo <ravelaso@outlook.com>
2024-10-29 08:40:08 +0100

    init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/pquerna/otp/totp"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <token>", os.Args[0])
+	}
+
+	secret := os.Args[1]
+	// Get the current time
+	now := time.Now()
+
+	// Generate the OTP
+	passcode, err := totp.GenerateCode(secret, now)
+	if err != nil {
+		log.Fatalf("Error generating OTP: %v", err)
+	}
+
+	fmt.Printf("Your OTP is: %s\n", passcode)
+	fmt.Print("Press 'Enter' to close...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
